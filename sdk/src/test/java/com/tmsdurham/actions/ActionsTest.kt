424740e8b0999ba10bea6e292372a10d6a1018e8
Patrick Jackson <patrick.jackson@ticketmaster.com>
2017-07-10 21:17:39 -0400

    spek framework working

--- sdk/src/test/java/com/tmsdurham/actions/ActionsTest.kt
+++ sdk/src/test/java/com/tmsdurham/actions/ActionsTest.kt
@@ -3,12 +3,9 @@ import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
-import org.junit.platform.runner.JUnitPlatform
-import org.junit.runner.RunWith
 
 val gson = Gson()
 
-@RunWith(JUnitPlatform::class)
 object ActionsTest: Spek({
     describe("Working test") {
         on(" running") {
